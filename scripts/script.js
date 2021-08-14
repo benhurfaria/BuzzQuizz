8310d35dcd912d60c4fb7a652288073eae3273b8
Ben Hur Faria <benhurfaria3@gmail.com>
2021-08-14 20:16:09 -0300

    mais ajustes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 function criarQuiz(){
-    const escondendo = document.querySelector(".tela1");
-    escondendo.classList.add("escondido");
+    const escondendoCriar = document.querySelector(".criar-quiz");
+    escondendoCriar.classList.add("escondido");
+    const escondendoQuizes = document.querySelector(".todos-os-quizes");
+    escondendoQuizes.classList.add("escondido");
+    const escondendoTodos = document.querySelector(".Todos");
+    escondendoTodos.classList.add("escondido");
     const aparecendo = document.querySelector(".screen-3");
     aparecendo.classList.remove("escondido");
 }
