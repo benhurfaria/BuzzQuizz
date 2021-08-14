23f5e05a0f003daf4c22010bec23efca83471ea5
Ben Hur Faria <benhurfaria3@gmail.com>
2021-08-14 19:37:44 -0300

    primeira parte do js

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+function criarQuiz(){
+    const escondendo = document.querySelector(".criar-quiz");
+    escondendo.classList.add("escondido");
+    const aparecendo = document.querySelector(".screen-3");
+    aparecendo.classList.remove("escondido");
+}
