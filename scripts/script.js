7866fded13f9ed9f3cc345839d607832d0ebc2e7
Ben Hur Faria <benhurfaria3@gmail.com>
2021-08-16 16:29:43 -0300

    ajustes

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,9 @@ function pegarQuizzes(){
     const promessa = axios.get(URL_OBTER_QUIZZES);
     promessa.then(CarregarQuizz);
 }
+
 pegarQuizzes();
+
 function CarregarQuizz(resposta){
     quizzes = resposta.data;
     console.log(quizzes);
@@ -35,4 +37,25 @@ function criarQuiz(){
 
     const aparecendo = document.querySelector(".screen-3");
     aparecendo.classList.remove("escondido");
+}
+
+function irParaQuiz(){
+    const escondendoCriar = document.querySelector(".criar-quiz");
+    escondendoCriar.classList.add("escondido");
+
+    const escondendoQuizes = document.querySelector(".todos-os-quizes");
+    escondendoQuizes.classList.add("escondido");
+
+    const escondendoTodos = document.querySelector(".Todos");
+    escondendoTodos.classList.add("escondido");
+
+    const aparecendo = document.querySelector(".titulo-quizz");
+    aparecendo.classList.remove("escondido");
+
+    const aparecendo2 = document.querySelector(".titulo-pergunta");
+    aparecendo2.classList.remove("escondido");
+
+    const aparecendo3 = document.querySelector(".respostas-unica");
+    aparecendo3.classList.remove("escondido");
+
 }
